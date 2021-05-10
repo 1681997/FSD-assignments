6b60e5e43a00b9c887e5790778f51cabf4791a34
Sima Gaikwad <simagaikwad1234@gmail.com>
2021-05-10 19:46:57 +0530

    Commit

---
+++ Assignment5/Front-End/src/app/employee.ts
@@ -0,0 +1,13 @@
+export class Employee {
+
+    constructor(
+        public id?: number,
+        public firstName?: string,
+        public lastName?: string,
+        public emailId?: string
+    ){}
+    
+}
+
+    
+
